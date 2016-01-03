b34f2240c7e94e319e6a12cdc7121e0ddfbe213c
Jeffrey Sarnoff <jeffreysarnoff@gmail.com>
2016-01-03 16:00:39 -0500

    Update FloatHigher.jl

--- src/FloatHigher.jl
+++ src/FloatHigher.jl
@@ -23,7 +23,7 @@ typealias Ball arb # types are capitalized in Julia
 typealias SystemNum Union{Float64,Float32,Float16,Int128,Int64,Int32,Int16}
 
 
-if isdefined(Main,:UseDigits30) || (!isdefined(Main,:UseDigits75) & !isdefined(Main,:UseDigits150) & !isdefined(Main,:UseDigits300))
+if isdefined(Main,:UseDigits30) || (!isdefined(Main,:UseDigits70) & !isdefined(Main,:UseDigits150) & !isdefined(Main,:UseDigits300))
 export Digits30
 
 immutable (Digits30) <: Real
@@ -36,16 +36,16 @@ TypeSym = :Digits30; RoundDigs=30; FmtStr="%0.30g"
 include("type.jl")
 end
 
-if isdefined(Main,:UseFloat256) && Main.UseFloat256==true
-export Float256
+if isdefined(Main,:UseDigits70) && Main.UseDigits70==true
+export Digits70
 
-immutable (Float256) <: Real
+immutable (Digits70) <: Real
    re::Ball
 end
-Real256 = ArbField(288)
-convert{T<:SystemNum}(::Type{Float256}, x::T) = (Float256)(Real256(x))
+Real70 = ArbField(255)
+convert{T<:SystemNum}(::Type{Digits70}, x::T) = (Digits70)(Real70(x))
 
-TypeSym = :Float256; RoundDigs=76; FmtStr="%0.76g"
+TypeSym = :Digits70; RoundDigs=70; FmtStr="%0.70g"
 include("type.jl")
 end
 
